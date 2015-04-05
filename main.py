fb5a4cd27638328bfaa27f6455245355f1a4ba7b
Mark Richards <mrichards39@hotmail.co.uk>
2015-04-05 22:41:51 +0100

    Adding recursive checks to find all possible combinations.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #imports
 import random
 import string
-
+import copy
 ##setup
 max_height = 4
 max_width = 4
@@ -11,28 +11,28 @@ dictionary_filename = "words.txt"
 
 def display_board(board):
 	header = ""
-	for y in range(0,max_width+2):
+	for x in range(0,max_width+2):
 		header+="# "
 	print header
 
 	for row in board:
 		current_line ="# "
-		for column_value in row:
-			current_line+=column_value+" "
+		for value in row:
+			current_line+=value+" "
 		current_line+="#"
 		print current_line
 
 	footer = ""
-	for y in range(0,max_width+2):
+	for x in range(0,max_width+2):
 		footer+="# "
 	print footer
 
 #procedure randomly generates a board to work with
 def randomize_new_board():
 	board = []
-	for row in range(0,max_width):
+	for column in range(0,max_width):
 		row_values = []
-		for column in range(0,max_height):
+		for row in range(0,max_height):
 			random_letter = random.choice(string.ascii_uppercase)
 			row_values.append(random_letter)
 		board.append(row_values)
@@ -40,18 +40,62 @@ def randomize_new_board():
 	return board
 
 #procedure to read from a text file a list of all possible words
-def load_dictionary():
+def load_dictionary_set():
 	dictionary_file = open(dictionary_filename,"r")
 	dictionary_string = dictionary_file.read()
-	dictionary = dictionary_string.split("\n")
+	dictionary = set(dictionary_string.split("\n"))
 	return dictionary
 
-def check_adjacent_tiles_for_words(board, current_string, x_pos, y_pos):
+#function returns all words found based on the current tile as a set
+def check_tiles_and_neighbours_for_words(board, dictionary_set, current_string, current_tile, visited):
+	#if current string has already been visited - base case to stop checking
+	x = current_tile[0]
+	y = current_tile[1]
+	
+	if visited[x][y]==True:
+		return set()
+	found_words = set([current_string]) if is_a_word(dictionary_set, current_string)==True else set()
+	# check neighbours if within bounds
 
+	adjacent_tiles = []
+	adjacent_tiles.append((x-1,y))
+	adjacent_tiles.append((x-1,y+1))
+	adjacent_tiles.append((x-1,y-1))
+	adjacent_tiles.append((x,y+1))
+	adjacent_tiles.append((x,y-1))
+	adjacent_tiles.append((x+1,y))
+	adjacent_tiles.append((x+1,y+1))
+	adjacent_tiles.append((x+1,y-1))
+
+	visited[x][y]=True
+	#get words in neighbouring tiles
+	for tile in adjacent_tiles:
+		#get smallest x/y value
+		smallest = min(tile)
+		largest = max(tile)
+		if smallest>=0 and largest<len(board):
+			tile_x = tile[0]
+			tile_y = tile[1]
+			current_letter = board[tile_x][tile_y]
+			new_proposed_string = current_string+current_letter
+			found_words = found_words.union(check_tiles_and_neighbours_for_words(board, dictionary_set, new_proposed_string,tile,visited))
+			if len(found_words)>0:
+				print found_words
+	return found_words
+
+def is_a_word(dictionary_set, word):
+	return True if check_word_score(dictionary_set,word)>0 else False
+
+#check if proposed word is a word, and return an applicable score based on length
+def check_word_score(dictionary_set, word):
+	if word in dictionary_set:
+		return len(word)
+	return 0
 
 board = randomize_new_board()
 display_board(board)
-words = load_dictionary()
+words = load_dictionary_set()
+
 # to find all possible solutions:
 # 1) iterate over each character as a starting tile
 # 2) with each tile, check if existing current list of characters is  word this empty if starting
@@ -59,9 +103,17 @@ words = load_dictionary()
 # 4) check adjacent vertical/horizontal/diagonal tiles, recursively perform 2-4 for all subsequent adjacent tiles 
 # until no possible word can be found on the current list of chracters
 # 5) Choose the next tile and 1-4 until all tiles have been checked
-
+#
+#initialise visited tiles
+visited_tiles = {}
+for x in range(0,len(board)):
+	visited_tiles[x]={}
+	for y in range(0,len(board)):
+		visited_tiles[x][y]=False
 for x in range(0,len(board)):
 	values = board[x]
-	for y in range(0,len(y_values)):
+	
+	for y in range(0,len(values)):
 		current_letter = values[y]
-		check_adjacent_files_for_words(board, current_letter,x,y)
+		print "Test",current_letter
+		check_tiles_and_neighbours_for_words(board, words, current_letter,(x,y), copy.deepcopy(visited_tiles))
