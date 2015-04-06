bc3d0f5d21a252e727d4fa6b17bbec8909ffad43
Mark Richards <mrichards39@hotmail.co.uk>
2015-04-06 23:13:28 +0100

    Cleaning up some comments and fixing some issues with the logic.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import string
 import copy
 import time
 
-##setup
+#setup
 max_height = 4
 max_width = 4
 
@@ -64,42 +64,40 @@ def randomize_new_board():
 	for column in range(0,max_width):
 		row_values = []
 		for row in range(0,max_height):
-			random_letter = random.choice(string.ascii_uppercase)
+			random_letter = random.choice(string.ascii_lowercase)
 			while random_letter in used_chars:
-				random_letter = random.choice(string.ascii_uppercase)
-				if random_letter=="Q":
-					random_letter= "QU"
+				random_letter = random.choice(string.ascii_lowercase)
+				#substitute Q with QU as intended in boggle
+			#if random_letter=="q":
+			#	random_letter= "qu"
 			row_values.append(random_letter)
 			used_chars[random_letter]=True
 		board.append(row_values)
 
 	return board
 
-#procedure to read from a text file a list of all possible words
+#procedure to read from a text file a list of all possible words as a set
 def load_dictionary_set():
 	dictionary_file = open(dictionary_filename,"r")
 	dictionary_string = dictionary_file.read()
 	dictionary = set(dictionary_string.split("\n"))
 	return dictionary
 
+#method maps all words to a nested dictionary of letters each corresponding prefix to words. Prefixs could be seen
 def make_prefix_tree(dictionary):
 	prefix_tree = {}
 	for word in dictionary:
 		current_subtree = prefix_tree
 		for letter in word:
 			#fetch child letter, if there is none then create one
-			prefix_entry = current_subtree.get(letter.upper())
+			prefix_entry = current_subtree.get(letter)
 			if prefix_entry is None:
-				current_subtree[letter.upper()] = {}
-				prefix_entry = current_subtree.get(letter.upper())
+				current_subtree[letter] = {}
+				prefix_entry = current_subtree.get(letter)
 			current_subtree = prefix_entry
 	return prefix_tree
 
-def get_size_of_max_string(array):
-	max_size = max(array, key=len)
-	return max_size
-
-#function returns all words found based on the current tile as a set
+#function returns all words found based on the current tile. Words are returned as a set
 def check_tiles_and_neighbours_for_words(board, dictionary_set, current_string, current_tile, visited, prefix_tree=None):
 	#if current string has already been visited - base case to stop checking
 	x = current_tile[0]
@@ -109,18 +107,21 @@ def check_tiles_and_neighbours_for_words(board, dictionary_set, current_string, 
 		return set()
 
 	found_words = set()
-	prev_letter = current_string[-1]
+	current_letter = current_string[len(current_string)-1]
+	prefix_subtree = None
 
 	#if using a prefix tree, check if any possible words begin with the current word
 	if prefix_tree is not None:
+		
+		prefix_subtree = prefix_tree.get(current_letter)
 		#back trace early if current string is not a prefix of the word to seek
-		prefix_subtree = prefix_tree.get(prev_letter)
 		if prefix_subtree is None:
 			return set()
-		#print prev_letter
-		if len(prefix_subtree)==0 and len(current_string)>2:
-			print current_string
-			return set([current_string])
+		if len(current_string)>2:
+			#If the current root is a word then add the word to the set
+			if is_a_word(dictionary_set, current_string)==True:
+				#print current_string
+				found_words.add(current_string)
 	else:	
 		found_words = set([current_string]) if is_a_word(dictionary_set, current_string)==True else set()
 
@@ -135,20 +136,18 @@ def check_tiles_and_neighbours_for_words(board, dictionary_set, current_string, 
 		letter = board[tile_x][tile_y]
 		new_proposed_string = current_string+letter
 		found_words = found_words.union(check_tiles_and_neighbours_for_words(board, dictionary_set, new_proposed_string,tile,copy.deepcopy(visited),prefix_subtree))
-
 	return found_words
 
-#performance - very poor
 def naive_implementation(board):
-	# to find all possible solutions:
-	# 1) iterate over each character as a starting tile
-	# 2) with each tile, check if existing current list of characters is  word this empty if starting
-	# 3) if the current list of characters is a word, add to found list and add to total points
-	# 4) check adjacent vertical/horizontal/diagonal tiles, recursively perform steps 2-4 for all subsequent adjacent tiles 
-	# until no possible word can be found on the current list of chracters
-	# 5) Choose the next tile and 1-4 until all tiles have been checked
 	words = load_dictionary_set()
-
+	# to find all possible solutions:
+	# 1) iterate over each tile as a starting character
+	# 2) with the current tile, check if this tile and subsequent adjacent tiles form any words
+	# 3) if the tile has been visited in the chain before, return an empty set 
+	# 4) if the current list of characters is a word, add to the found list and add to the total points
+	# 5) check adjacent vertical/horizontal/diagonal tiles, recursively perform steps 2-4 for all subsequent adjacent tiles 
+	# 	 until no possible word can be found on the current list of characters
+	# 6) choose the next tile and repeat 1-5 until all tiles on the board have been checked
 	visited_tiles = {}
 	for x in range(0,len(board)):
 		visited_tiles[x]={}
@@ -161,9 +160,19 @@ def naive_implementation(board):
 			current_letter = values[y]
 			check_tiles_and_neighbours_for_words(board, words, current_letter,(x,y), copy.deepcopy(visited_tiles))
 
-#method as above in the pruned solution, only th
+#method uses backtracing/pruning so that recursive calls are halted when the current string is not a substring of any possible word
 def pruned_implementation(board):
 	words = load_dictionary_set()
+	# to find all possible solutions:
+	# 1) iterate over each tile as a starting character
+	# 2) with each tile, check if this tile and subsequent adjacent tiles form any words
+	# 3) if the tile has been visited in the chain before, return an empty set
+	# 4) in check_tiles_and_neighbours_for_words use a prefix tree to determine if the current chain of a string is a valid prefix to any possible word
+	# 5) if the current string called in check_tiles_and_neighbours is not a prefix to any posible word then return an empty set. No possible words exist under this prefix.
+	# 6) if the current string called in check_tiles_and_neighbours is a prefix and in fact is a real word then add this to found list
+	# 7) check adjacent vertical/horizontal/diagonal tiles, recursively perform steps 2-6 for all subsequent adjacent tiles 
+	# 	 until no possible word can be found on the current list of characters
+	# 8) choose the next tile and repeat 1-7 until all tiles on the board have been checked
 	prefix_tree = make_prefix_tree(words)
 
 	visited_tiles = {}
@@ -171,17 +180,28 @@ def pruned_implementation(board):
 		visited_tiles[x]={}
 		for y in range(0,len(board)):
 			visited_tiles[x][y]=False
-
+	
+	all_found = set()
 	for x in range(0,len(board)):
 		values = board[x]
 		for y in range(0,len(values)):
 			current_letter = values[y]
-			check_tiles_and_neighbours_for_words(board, words, current_letter, (x,y), copy.deepcopy(visited_tiles),prefix_tree)				
+			all_found = all_found.union(check_tiles_and_neighbours_for_words(board, words, current_letter, (x,y), copy.deepcopy(visited_tiles),prefix_tree))			
+	return all_found
 
-#naive_implementation(board)
+# main code
 board = randomize_new_board()
 display_board(board)
 start = time.time()
-pruned_implementation(board)
+solutions = pruned_implementation(board)
 end = time.time()
 print "Time Elapsed: ",end - start
+max_score = 0
+total_score = 0
+for sol in solutions:
+	max_score = len(sol) if len(sol)>max_score else max_score
+	total_score+=len(sol)
+	print sol
+print "Total Highest Score: ",total_score
+print "Highest Scoring word: ",max_score
+	
