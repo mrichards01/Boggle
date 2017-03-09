9cf0afd7d8c841fc54273ce7d6703ccd3835dfd4
Mark Richards <Mark Richards>
2017-03-09 23:39:30 +0000

    Changing main.py to show total solve time with initialisation time and including a stage by stage prompt.

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import time
-import boggle_solver
+from boggle_solver import BoggleSolver
 
 #file contents taken from http://www-01.sil.org/linguistics/wordlists/english/ for private use
 dictionary_filename = "words.txt"
 
-solver = boggle_solver.BoggleSolver(4, 4, dictionary_filename)
-solver.display_board()
-# time solution
-start = time.time()
-solver.solve()
-end = time.time()
-print ("Time Elapsed: ",end - start)
+while True:
+	input ("\n\rPress enter to generate a new Boggle Board\n\r")
+	init_start = time.time()
+	solver = BoggleSolver(4, 4, dictionary_filename)
+	init_end = time.time()
+	solver.display_board()
+	input("\n\rPress enter to show solutions and highest possible scores\n\r")
+	
+	start = time.time()
+	solver.solve()
+	end = time.time()
+	print ("\n\rTime Elapsed to initialise and read dictionary: ",init_end - init_start)
+	print ("Time Elapsed to find solutions: ",end - start)
+	print ("Total Time to solve: ",(init_end- init_start)+(end-start))
 
